e262b3535719da87af3ce21e416d15039a21bdf2
naeyanika <40598758+naeyanika@users.noreply.github.com>
2024-08-06 15:20:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ if uploaded_files:
         df_kdp = dfs['KDP.xlsx']
 
 # Filter KDP
+df_na = df_kdp.dropna(subset=['Cr PRR'])
 df_filter_kdp = df_kdp[df_kdp['Cr PRR'] > 0].copy()
 
 st.write ("KDP Filter")
